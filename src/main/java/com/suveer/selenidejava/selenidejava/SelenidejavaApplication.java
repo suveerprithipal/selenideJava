f8a8e34ff6aa50a31ae68259b8e1f62bc8565b52
Suveer Prithipal <suveerprithipal@Suveer.local>
2017-11-22 11:35:55 +1300

    Base app impl.

---
+++ src/main/java/com/suveer/selenidejava/selenidejava/SelenidejavaApplication.java
@@ -0,0 +1,12 @@
+package com.suveer.selenidejava.selenidejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelenidejavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelenidejavaApplication.class, args);
+	}
+}
