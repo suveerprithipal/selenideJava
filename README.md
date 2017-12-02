869bf0e0dab33c7fb80e452c722f77e7cc0a68a1
Suveer Prithipal <suveerprithipal@Suveer.local>
2017-12-03 12:08:37 +1300

    Added readMe

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# selenideJava
-WebDriver example using selenide, a Selenium wrapper
+# Selenide + Java + Junit + Page Object Models
+An example application using selenide, a selenium wrapper.
+We will utilize a page object model dev pattern to layer out the test.
+
+The test is run without springboot or a cucumber context.
+We run the test using JUnit.
+
+###Benefits: 
+This frontend functional test runs sub 10 seconds. 
--- src/test/java/com/suveer/selenidejava/selenidejava/GoogleTest.java
+++ src/test/java/com/suveer/selenidejava/selenidejava/GoogleTest.java
@@ -1,18 +1,14 @@
 package com.suveer.selenidejava.selenidejava;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
 public class GoogleTest {
-
-
   @Test
   public void googlePageTest(){
 
-    GooglePage googlePage = open("http://www.google.co.nz",GooglePage.class);
+    GooglePage googlePage = open("http://www.google.com",GooglePage.class);
     SearchResultsPage searchResultsPage = googlePage.searchGoogle("selenide");
 
     searchResultsPage.checkResultsSize(9);
