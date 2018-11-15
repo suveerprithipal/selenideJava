5d7b04ba239bcaee7eac896d3278147320cb215a
SuveerAtGentrack <36015424+SuveerAtGentrack@users.noreply.github.com>
2018-11-15 15:33:19 +1300

    pointing to aws

---
+++ src/main/java/com/suveer/selenidejava/selenidejava/selenium/ServletClass.java
@@ -0,0 +1,10 @@
+package com.suveer.selenidejava.selenidejava.selenium;
+
+import com.thoughtworks.selenium.Selenium;
+import com.thoughtworks.selenium.webdriven.SeleniumMutator;
+
+public class ServletClass {
+
+
+
+}
